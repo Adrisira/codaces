d38eacc0133213c1b43a017520dca0cbbbeed4fd
Adrián Sigüenza Ramírez <88464152+Adrisira@users.noreply.github.com>
2024-11-23 16:52:48 +0100

    first commit

---
+++ src/main/java/com/api/codaces/CodacesApplication.java
@@ -0,0 +1,13 @@
+package com.api.codaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodacesApplication.class, args);
+	}
+
+}
